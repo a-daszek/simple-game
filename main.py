b29ea8daf311bf308dc91ea709fbf0fc01cb49a7
Anna Daszyńska <anna.daszynska@protonmail.com>
2023-06-19 20:44:48 +0200

    added character class and coin class

--- main.py
+++ main.py
@@ -19,9 +19,7 @@ class Platform():
         self.area = pygame.Rect(self.x, self.y, self.width, self. height)
 
     def draw_platform(self):
-        random_chance = random.randint(1, 6)
-        if random_chance == 1:
-            pygame.draw.rect(window, self.color, self.area)
+        pygame.draw.rect(window, self.color, self.area)
 
 
 class Coin():
@@ -32,9 +30,44 @@ class Coin():
         self.color = "yellow"
         self.is_visible = True
 
+    def draw_coin(self):
+        if self.is_visible == True:
+            pygame.draw.circle(window, self.color, (self.x, self.y), self.r)
+
+
+class Character():
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.area = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.graphics = pygame.image.load("foxy.png")
+        self.graphics = pygame.transform.scale(self.graphics, (60, 60))
+
+    def draw_character(self):
+        window.blit(self.graphics, (self.x, self.y))
+
+    def move_left(self):
+        self.x = self.x - 5
+        self.area = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def move_right(self):
+        self.x = self.x + 5
+        self.area = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def move_down(self):
+        self.y = self.y + 5
+        self.area = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def move_up(self):
+        self.y = self.y - 5
+        self.area = pygame.Rect(self.x, self.y, self.width, self.height)
+
+
+player = Character(50, 150, 60, 60)
 
 platforms = []
-
 
 for i in range(0, 14):
     platforms.append(Platform(random.randint(20, 500), random.randint(20, 500),
@@ -42,6 +75,11 @@ for i in range(0, 14):
     # depends on your luck (how the platforms generate)... so, good luck XD (I'll change this, no worries)
 
 platforms.append(Platform(0, 570, width, 30))
+
+coins = []
+
+for i in range(0, 3):
+    coins.append(Coin(random.randint(60, 450), random.randint(60, 450), 20))
 
 while True:
     for event in pygame.event.get():
@@ -51,5 +89,10 @@ while True:
 
     for p in platforms:
         p.draw_platform()
+
+    for c in coins:
+        c.draw_coin()
+
+    player.draw_character()
 
     pygame.display.update()
